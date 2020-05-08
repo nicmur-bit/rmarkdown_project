243673ced004c935c95356d0a677e3e6aafb479d
Murphy <mur324@csiro.au>
2020-05-08 15:04:48 +1000

    playing with density outputs

--- scripts/bdm_lf.R
+++ scripts/bdm_lf.R
@@ -1,16 +1,28 @@
 library(tidyverse)
 
-bdm_lf <- read_csv("data/bdm_length_frequency.csv") %>% 
-filter(!is.na(length), !is.na(weight))
+bdm_lf <- read_csv("data/bdm_length_frequency.csv") 
 
-bdm_lf %>% 
-  group_by(species)
-
-bdm_echinites <- filter(bdm_lf, species == "Actinopyga echinites") %>% 
-  group_by(year)
-
+bdm_echinites <- bdm_lf %>% 
+  filter(str_detect(species, 'echinites|echin|tes')) +
+  
+  
 bdm_plot1 <- bdm_echinites %>% 
   ggplot(aes(x = length)) +
-  geom_bar(group = year)
+  geom_bar()+
+  facet_wrap(~year)
+ 
 
+#filter(!is.na(length), !is.na(weight))
+#filter(bdm_lf, species == "Actinopyga echinites") 
+#group_by(year)
 
+bdm_density <- read_csv("data/bdm_density_average_sea cucumber_only.csv")
+
+bdm_whitmaei <- bdm_density %>% 
+  select('holothuria_whitmaei', 'zone', 'samocc', 'strata')  %>% 
+  group_by(zone)
+  
+  
+ggplot(bdm_whitmaei, aes(x= samocc)) +
+  geom_density() +
+  facet_wrap(~ zone)
